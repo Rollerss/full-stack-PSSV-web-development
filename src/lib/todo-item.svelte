6dae0446c1d1a27c1a2e3a17ba107ba26bd01be6
Rollerss <oneouthere@gmail.com>
2021-12-21 08:12:26 +0000

    Add Get and Post endpoints

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,10 +1,14 @@
+<script lang="ts">
+    export let todo: Todo;
+</script>
+
 <div class="todo" class:done={false}>
     <form action="" method="">
         <input type="hidden" name="done" value="">
         <button aria-label="Mark done/not done" class="toggle"></button>
     </form>
     <form action="" method="" class="text">
-        <input type="text"/>
+        <input type="text" value="{todo.text}"/>
         <button aria-label="Save todo" class="save"></button>
     </form>
     <form action="" method="">
